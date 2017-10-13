51c4ebb332b140357dfcfa243a9e735707c8a1b9
merciewaf <mrcwafula@gmail.com>
2017-10-13 14:38:04 +0300

    Blog working well

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,8 @@ class Post < ApplicationRecord
 	has_many :comments
 	attr_accessor :content, :name, :title
 	
-	
+	validates :name,  :presence => true
+  validates :title, :presence => true,
+                    :length => { :minimum => 5 }
+  has_many :comments, :dependent => :destroy
 end
